388040de17e7e8804f93f5fbe81322f471994811
dickkouwenhoven <66950599+dickkouwenhoven@users.noreply.github.com>
2025-07-15 09:15:42 +0200

    Create settings.gradle.kts

---
+++ annotations/settings.gradle.kts
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2022 Dick Kouwenhoven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+logger.info("\n")
+logger.info("    ==========                      Start of settings.gradle.kts from annotations                      ==========\n")
+logger.info("Step 1 : plugin settings.")
+plugins {
+    alias(libs.plugins.kmpBuildLogicSettingsPlugin)
+}
+
+logger.info("Step 1 : 'rootProject name' setting.")
+val mainRootProjectName: String = System.getProperty("kmpbuildlogic.properties.root.project.name")
+val kmpbuildlogicAnnotationsProjectName =
+    mainRootProjectName +
+        "-" +
+        "Annotations
+rootProject.name = kmpbuildlogicAnnotationsProjectName
+
+logger.info("\n")
+logger.info("    ==========                       End of settings.gradle.kts from annotations                       ==========\n")
